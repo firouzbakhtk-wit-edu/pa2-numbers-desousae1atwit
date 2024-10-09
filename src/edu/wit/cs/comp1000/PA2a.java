0423267e683f5ebb214affe2b853d3e49d6a499d
Ethan DeSousa <desousae1@wit.edu>
2024-10-09 16:05:47 -0400

    Final Code

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,114 @@
 package edu.wit.cs.comp1000;
+/**
+ *  Solution to the third programming assignment.
+ *  When it runs it sum of non and positive and all numbers, 
+ *  also average of non and positive and all numbers
+ *  to the terminal.
+ *  
+ *  @author Ethan DeSousa
+ */
+import java.util.Scanner;
 
-// TODO: document this class
-public class PA2a {
-
-	// TODO: document this function
+public class PA2a {			// Finds sum of non and positive and all numbers, also average of non and positive and all numbers
+	
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		int num1, num2, num3, num4, num5;		// 5 user inputed numbers
+		int posNum = 0; 						// variable for total positive numbers
+		int nonposNum = 0; 						// variable for total non positive numbers
+		int sumNum = 0;							// variable for total numbers
+		int pos = 0;							// counter for positive numbers
+		int neg = 0;							// counter for non positive numbers
+		double avgPos, avgNeg, avgTotal;		// variables for average postive, non postive and total values
+		
+		
+		System.out.printf("Enter five whole numbers: ");		// User input
+		num1 = input.nextInt();
+		num2 = input.nextInt();
+		num3 = input.nextInt();
+		num4 = input.nextInt();
+		num5 = input.nextInt();
+		
+		if (num1 > 0) {						//If statement to determine positive or non positive inputs
+			posNum = posNum + num1;			// If positive add to total positive number
+			pos++;							// while also adding to positive or non positive counter
+		}
+		else {
+			nonposNum = nonposNum + num1;
+			neg++;
+		}
+		if (num2 > 0) {
+			posNum = posNum + num2;
+			pos++;
+		}
+		else {
+			nonposNum = nonposNum + num2;
+			neg++;
+		}
+		if (num3 > 0) {
+			posNum = posNum + num3;
+			pos++;
+		}
+		else {
+			nonposNum = nonposNum + num3;
+			neg++;
+		}
+		if (num4 > 0) {
+			posNum = posNum + num4;
+			pos++;
+		}
+		else {
+			nonposNum = nonposNum + num4;
+			neg++;
+		}
+		if (num5 > 0) {
+			posNum = posNum + num5;
+			pos++;
+		}
+		else {
+			nonposNum = nonposNum + num5;
+			neg++;
+		}
+		sumNum = num1 + num2 + num3 + num4 +num5;		// calculates sum of all numbers
+		avgTotal = sumNum / 5.0;						// calculates average of all numbers
+		if (pos > 0) {									// If statement to find the averages without doing division by 0
+			avgPos = (posNum * 1.0) / pos;
+		}
+		else {
+			avgPos = 0;
+		}
+		if (neg > 0) {
+			avgNeg = (nonposNum * 1.0) / neg;
+		}
+		else {
+			avgNeg = 0;
+		}
+		
+		if(pos == 1 && neg > 1) {					// if statement to determine how output should look										
+			System.out.printf("The sum of the %d positive number: %d%n", pos, posNum);
+			System.out.printf("The sum of the %d non-positive numbers: %d%n", neg, nonposNum);
+			System.out.printf("The sum of the 5 numbers: %d%n",sumNum);
+			System.out.printf("The average of the %d positive number: %.2f%n", pos, avgPos);
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", neg, avgNeg);
+			System.out.printf("The average of the 5 numbers: %.2f%n", avgTotal);
+		}
+		else if (pos > 1 && neg == 1){
+			System.out.printf("The sum of the %d positive numbers: %d%n", pos, posNum);
+			System.out.printf("The sum of the %d non-positive number: %d%n", neg, nonposNum);
+			System.out.printf("The sum of the 5 numbers: %d%n",sumNum);
+			System.out.printf("The average of the %d positive numbers: %.2f%n", pos, avgPos);
+			System.out.printf("The average of the %d non-positive number: %.2f%n", neg, avgNeg);
+			System.out.printf("The average of the 5 numbers: %.2f%n", avgTotal);
+		}
+		else {
+			System.out.printf("The sum of the %d positive numbers: %d%n", pos, posNum);
+			System.out.printf("The sum of the %d non-positive numbers: %d%n", neg, nonposNum);
+			System.out.printf("The sum of the 5 numbers: %d%n",sumNum);
+			System.out.printf("The average of the %d positive numbers: %.2f%n", pos, avgPos);
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", neg, avgNeg);
+			System.out.printf("The average of the 5 numbers: %.2f%n", avgTotal);
+		}
+		
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,45 @@
 package edu.wit.cs.comp1000;
-
-// TODO: document this class
-public class PA2b {
-
-	// TODO: document this function
+/**
+ *  Solution to the third programming assignment.
+ *  When it runs it outputs "Hello World!" to the
+ *  terminal.
+ *  
+ *  @author Ethan DeSousa
+ */
+import java.util.Scanner;
+ 
+public class PA2b { 		// Calculates discriminant then idicates number of roots 
+	
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		double a,b,c;			// values inputed of quadratic function
+		double dis = 0;			// discriminant
+		double root1 = 0;       
+		double root2 = 0;
+		System.out.printf("Enter a b c: ");
+		a = input.nextDouble();
+		b = input.nextDouble();
+		c = input.nextDouble();
+		
+		dis = (b*b) - (4 * a *c);		// Calculates discriminant
+		
+		if (dis < 0) {
+			System.out.printf("Roots: imaginary%n");		//If statement to use discriminant to determine roots
+		}
+		else if (dis == 0) {
+			root1 = (-1.0 * b) / (2.0 * a);					// Finds the one root
+			System.out.printf("Root: %.2f%n", root1);
+		}
+		else {
+			root1 = (-b - Math.sqrt(dis)) / (2 * a);		// Finds the two roots
+			root2 = (-b + Math.sqrt(dis)) / (2 * a);
+			if (root1 > root2) {
+			    double temp = root1;
+			    root1 = root2;
+			    root2 = temp;
+			}
+			System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
+		}
 	}
 
 }
